95f171638d28c9790cb03ccf6f5fa0050821bb1b
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-25 18:25:55 +0530

    API(Application Programming Interface)using python

---
+++ api.py
@@ -0,0 +1,27 @@
+'''
+Name : API Documentation File
+Input : Float,Integer
+Output: Float
+Discription: API Documentation File using Python Program
+Author: Prasad Dangare
+'''
+
+
+def add(x, y):
+    """
+    this function takes two numbers and finds their sum.
+    it display the sum as result.
+    """
+    print("sum= ", (x+y))
+    
+def message():
+    """
+    this function display a message.
+    this is a welcome message to the user.
+    """
+    print("welcome to python")
+    
+# now call the functions
+
+add(10, 25)
+message()
