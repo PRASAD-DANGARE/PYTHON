d5d5cdf401236dbd5afaef1b884cf0af6ca5644c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-25 18:22:18 +0530

    Accept Two Numbers

---
+++ sum.py
@@ -0,0 +1,14 @@
+# Python Program To Accept Two Numbers And Find There Sum.
+'''
+Function Name   :  Accept Two Numbers From User
+Function Date   :  23 Aug 2020
+Function Author :  Prasad Dangare
+Input           :  Integer,Integer
+Output          :  Integer
+'''
+x = int(input('Enter First Number:'))
+y = int(input('Enter Second Number:'))
+
+print('The Sum Of {} and {} is {}'.format(x,y , x+y))
+
+# we also use print('The Sum Of ',x,'and',y', is',x+y) Display the addition of them
