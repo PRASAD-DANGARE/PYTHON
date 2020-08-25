420efa21e67277b709cf1712a1bec56e78f36613
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-25 18:44:34 +0530

    Sum And Product Of Two Numbers

---
+++ product.py
@@ -0,0 +1,26 @@
+# Python Program To Find Sum And Product Of Two Numbers
+'''
+Function Name   :  Find Sum And Product
+Function Date   :  25 Aug 2020
+Function Author :  Prasad Dangare
+Input           :  Integer
+Output          :  Integer
+'''
+
+x = int(input('Enter First Number:'))
+y = int(input('Enter Second Number'))
+
+# Display Sum
+
+print('The Sum Of {} and {} is {}'.format(x,y , x+y))
+
+# Display Product
+
+print('The Product of {} and {} is {}'.format(x,y , x+y))
+
+# Display Both Sum And Product
+
+print('The Sum of {0} and {1} is {2} and product of {0} and {1} is {3}'.format(x,y , x+y , x*y))
+
+
+
