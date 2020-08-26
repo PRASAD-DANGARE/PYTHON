0e33f4096a560afc0b128839c3746f083811141a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-26 12:44:46 +0530

    Accept 3 Numbers From Keyboard.

---
+++ program.py
@@ -0,0 +1,4 @@
+# python Program To Accept 3 Integers In The Same Line And Display Their Sum.
+
+var1, var2, var3 = [int(x)for x in input("Enter Three Numbers With A Space In Between:").split()] # Give Space In Input 
+print('Sum =', var1 + var2 + var3) # It Display The Sum Of 3 Numbers
