d7b5af56fd1d2ee7a873d0a9b00b81dc88b6f20b
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-26 12:55:31 +0530

    Accept  A Group Of String

---
+++ string.py
@@ -0,0 +1,11 @@
+# Python Program To Accept A Group Of String Seprated By Commas
+'''
+Function Name   :   Accept A Group Of String
+Function Date   :   26 Aug 2020
+Function Author :   Prasad Dangare
+Input           :   Charactor
+Output          :   Charactor
+'''
+
+lst = [x for x in input('Enter String:').split(',')] 
+print('You Entered :\n', lst)
