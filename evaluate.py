99ecee9c4f140860002f3f7eded3e9b57c9b279a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-26 13:05:41 +0530

    Evaluating An Mathematical Expression

---
+++ evaluate.py
@@ -0,0 +1,12 @@
+# Evaluating An Expression By User
+'''
+Function Name    :  Evaluate Mathematical Expression
+Function Date    :  26 Aug 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer,Integer
+Output           :  Integer
+'''
+# Using eval() Function Along With input() Function
+
+x = eval(input("Enter An Expression:"))
+print("Result = %d" %x)
