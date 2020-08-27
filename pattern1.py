5ce51a1bd1a5f09df3e63ad40edb4bb898c04e6c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-27 17:23:04 +0530

    Right Angled Triangular

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+# Python Program That Display Stars In Right Angle Triangular From Using Nested Loop
+
+'''
+Function Name    :  Pattern Printing
+Function Date    :  27 Aug 2020
+Function Author  :  Prasad Dangare
+Pattern Name     :  Right Angle Triangular
+'''
+
+for i in range(1, 11): # Display Rows 10
+   for j in range (1, i+1): # Number Of Stars = Row Numbers
+     print ('* ', end ='')
+   print() # Display In Row By Row
