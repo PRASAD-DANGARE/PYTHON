9da3bf5288a23ccfdaed45a7e050ac724af1ef0d
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-27 13:26:16 +0530

    A Given Number Is In Between 1 To 10

---
+++ test.py
@@ -0,0 +1,8 @@
+# python program to test whether a given number is between 1 to 10 
+
+x = int(input("Enter a Number:"))
+if x>=1 and x<= 10:
+    print("You Typed", x ,"Which is in Between 1 and 10")
+else:
+    print("You Typed", x ,"Which is not in Bewteen 1 and 10")
+    
