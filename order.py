66e5492b586554c1330bba8121ee153bdc357d46
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-08-27 17:00:09 +0530

    Ascending And Descending Order

---
+++ order.py
@@ -0,0 +1,24 @@
+# Python Program To Display Numbers From 10 To 1 In Ascending Order And Descending Order
+'''
+Function Name    :  Display Numbers From 10 To 1
+Function Date    :  27 Aug 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Integer
+'''
+
+
+for x in range(10, 0, -1): print(x)
+'''
+In Descending Order 10 Is The Starting Number. 
+0 Is The Number Till Rotate.
+-1 Is The Value Of Decrement Till n-1.
+'''
+print("\n")
+
+for y in range(1, 11, 1): print(y)
+'''
+1 Is The Number Of Starting Point. 
+11 Is The Rotation Number 1 to 10.
+1 Is The Increment Value Till n-1.
+'''
