9881dbddb7e51d659b62d16ff50ccc2065ef4652
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-03 16:58:01 +0530

    Identify Types Of Character Entered By The Users

---
+++ characters.py
@@ -0,0 +1,43 @@
+# Python Program To Know The Types Of Characters Entered By The User.
+
+'''
+Function Name    :  Identify Types Of Character Entered By Users . 
+Function Date    :  2 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  Integer 
+'''
+
+str = input('Enter A Character : ')
+print("\n")
+
+ch = str[0] # Takes Only The 0th Characters Into ch
+
+# Test ch
+
+if ch.isalnum():
+    print('It Is Alphabet Or Numeric Character')
+    print("\n")
+    
+    if ch.isalpha():
+        print('It Is An Alphabet')
+        print("\n")
+        
+        if ch.isupper():
+            print('It Is Capital Letter')
+            print("\n")
+            
+        else:
+            print('It Is LowerCase Letter')
+            print("\n")
+    else:
+        print('It Is A Numeric Digit')
+        print("\n")
+
+elif ch.isspace():
+        print('It Is A Space')
+        print("\n")
+
+else:
+    print('It May Be A Special Character')
+    print("\n")
