641e930fbeec4ffe04df633fb4d6beadcdc38505
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-04 19:16:31 +0530

    A Function To Add Two Numbers

---
+++ function.py
@@ -0,0 +1,21 @@
+# python program A Function That Accepts Two Values Finds Their Sum
+
+'''
+Function Name    :  A Function To Add Two Numbers . 
+Function Date    :  4 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Float
+'''
+
+def sum(a, b):
+    """ This Function Finds Sum Of Two Numbers """
+    
+    c = a + b
+    print('\n Sum = ', c)
+    
+# Call The Function
+
+sum(10, 15)
+sum(1.5, 10.75) # Call Second Time
+print("\n")
