c214569fa8a321a48721b3841a0590b736ed3d0e
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-04 19:29:03 +0530

    Sum Of Two Numbers And Return The Result

---
+++ function2.py
@@ -0,0 +1,24 @@
+# python program To Find Sum Of Two Numbers And Return The Result From Functions
+
+'''
+Function Name    :  Sum Of Two Numbers And Return The Result. 
+Function Date    :  4 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Integer
+'''
+
+def sum(a, b):
+    """ This Function Finds Sum Of Two Numbers """
+    
+    c = a + b
+    return c # Return Result
+    
+# Call The Function
+
+x = sum(10, 15)
+print('\n The Sum Is : ', x)
+
+y = sum(1.5, 10.75)
+print('The Sum Is : ', y)
+print("\n")
