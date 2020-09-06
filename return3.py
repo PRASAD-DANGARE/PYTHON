8cba973f76900c5d49d9e6a18808b43953c9aca5
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-06 17:15:30 +0530

    How To Assign A Function To A Variable

---
+++ return3.py
@@ -0,0 +1,17 @@
+# Python Program To See How To Assign A Function To A Variable
+
+'''
+Function Name    :  How To Assign A Function To A Variable. 
+Function Date    :  6 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String 
+'''
+
+def display(str):
+    return 'Hai ' + str
+
+# Assign Function To Variable X
+
+x = display("Prasad")
+print(x) 
