01ab3aeab0e038f8b6fcea51c581b8fd2b3a0252
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-07 19:23:18 +0530

    Use Position Argument In A Function

---
+++ Positionalarg.py
@@ -0,0 +1,19 @@
+# Python Program To Understand The Position Argument Of A Function
+
+'''
+Function Name    :  Use Position Argument In A Function . 
+Function Date    :  6 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String  
+'''
+
+def attach(s1, s2):
+    """ To Join s1 And s2 And Display Total String """
+    
+    s3 = s1 +s2
+    print('Total String : ' + s3)
+    
+# Call Attach And Pass 2 Strings
+
+attach('Prasad ', 'Dangare')
