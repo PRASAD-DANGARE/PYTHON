824e47d388bccc52695da3f7fb4f924d9979a48d
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-08 22:56:24 +0530

    Lambda Function It Returns Square Root Of A Number

---
+++ lambda.py
@@ -0,0 +1,13 @@
+# Python Program To Create A Lambda Function That Returns A Square Value Of A Given Number
+
+'''
+Function Name    :  Lambda Function It Returns Square Root Of A Number. 
+Function Date    :  8 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Integer  
+'''
+
+f = lambda x: x*x # f Is The Lambda Function 
+value = f(5) # Call Lambda Function
+print('square of 5 = ', value) # Display Result
