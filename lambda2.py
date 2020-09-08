ccec6c78453ba7d4f2dbfe06d87294312b4bb02f
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-08 23:03:08 +0530

    Lambda Function To Calculate Sum Of Two Numbers

---
+++ lambda2.py
@@ -0,0 +1,13 @@
+# Python Program Using Lambda Function To Calculate Sum Of Two Numbers
+
+'''
+Function Name    :  Lambda Function To Calculate Sum Of Two Numbers. 
+Function Date    :  8 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Float
+Output           :  Float
+'''
+
+f = lambda x, y: x + y # Write Lambda Function
+result = f(1.55, 10) # Call Lambda Function
+print('sum = ', result) # Display Result 
