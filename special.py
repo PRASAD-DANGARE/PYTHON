ce952f3f24f4210faf9763aca58946bb1b822c0a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-09 17:29:16 +0530

    Use The Special_Name_Variable

---
+++ special.py
@@ -0,0 +1,19 @@
+# python program using special_name_variable
+
+'''
+Function Name    :  Use The Special_Name_Variable.
+Function Date    :  9 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String,Integer
+Output           :  String
+'''
+
+def display():
+    print('Hello Python!')
+    
+    if __name__ == '__main__' :
+        display() # call display function
+        print('This Code Is Run As A Program')
+    else: 
+        print('This Code Is Run As A Module')
+        
