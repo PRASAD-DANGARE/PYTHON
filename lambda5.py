439b3f900a9d3abf4b64b2fa050e6f466bd528c6
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-09 12:31:34 +0530

    Square Of Elements In A List Using Map()

---
+++ lambda5.py
@@ -0,0 +1,13 @@
+# Python Program A Lambda Function That Returns Square Of Elements In A List Using map() Function
+
+'''
+Function Name    :  Square Of Elements In A List Using Map(). 
+Function Date    :  9 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Integer
+'''
+
+lst = [1,2,3,4,5]
+lst1 = list(map(lambda x: x*x, lst))
+print(lst1) 
