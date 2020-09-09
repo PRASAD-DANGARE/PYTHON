94da8994c68c87cc8af3710940e2b201e73dc432
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-09 17:33:39 +0530

    Use The Special_Name_Variable
    This program special2.py is Run Along With special.py , When We Compile special2.py Then We Get Print Statement Of special.py  In special2.py Program As A Module

---
+++ special2.py
@@ -0,0 +1,11 @@
+# python program that import the previous python program as a module (special.py)
+# in this program special2.py that import the previous python program as a module(special.py)
+
+"""This program special2.py is Run Along With special.py 
+    When We Compile special2.py Then We Get Print Statement Of special.py  In special2.py Program As A Mobule
+"""
+
+import special
+special.display()
+
+
