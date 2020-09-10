cefe573c7628af4bd59e1e987c48224b0a9ce1c0
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-10 20:50:21 +0530

    Aliasing In List.

---
+++ aliasing.py
@@ -0,0 +1,14 @@
+# Python Program To Understand The Use Of Aliasing In List
+
+'''
+Function Name    :  Alising In List.
+Function Date    :  10 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+Output           :  Integer
+'''
+
+x = [10,20,30,40,50]
+y = x # x is aliased as y
+print(x)
+print(y) 
