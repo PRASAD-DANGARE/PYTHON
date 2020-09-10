3e2ac845f42b6bd63719a6541bb09244e55d3bba
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-10 20:14:26 +0530

    Create List With Different Types Of Elements

---
+++ list.py
@@ -0,0 +1,31 @@
+# Python Program To Create Lists With Different Types Of Elements
+
+
+'''
+Function Name    :  Create List With Different Types Of Elements.
+Function Date    :  10 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String,Integer
+Output           :  String
+'''
+
+num = [10,20,30,40,50]
+print('Total Lists = ', num) # Display Total List
+print('First = %d, Last = %d' % (num[0], num[4])) # Display First And Last Elements
+print("\n")
+
+
+# Create A List With Strings
+
+names = ["Prasad", "Shubham", "Shiva", "Hritik"]
+print('Total List =', names) # Display Entire List
+print('First = %s, Last = %s' % (names[0], names[3])) # Display First And Last Elements
+print("\n")
+
+
+# Create A List With Different Elements
+
+x = [10,20,10.5,2.55,"Vishal", 'Sakshi']
+print('Total List = ', x) # Display Entire List
+print('First = %d, Last = %s' % (x[0], x[5])) # Display First And Last Elements
+print("\n")
