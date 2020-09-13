ce4cc069902fd86eeee6c35e1820bd6329969ef3
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-13 18:08:00 +0530

    Create Dictionary With Employee DetailsAndRetrieve

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+# Python Program To Create A Dictionary With Employees Details And Retrieve The Value Upon Giving The Keys
+
+'''
+Function Name    :  Create Dictionary With Employees Details And Retrieve It.
+Function Date    :  13 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String,Integer
+Output           :  String,Integer
+'''
+
+'''
+Create A Dictionary With Emplpyees Details
+Here 'Name' Is Key And 'Prasad' Is Its Value
+'Id' Is Key And 10 Is Its Value
+'Salary' Is Key And 100 Is Its Value
+'''
+
+dict = {'Name': 'Prasad', 'Id': 10, 'Salary': 100}
+
+# Access Value By Giving Key
+
+print('Name Of Employee = ', dict['Name'])
+print('Id Number = ', dict['Id'])
+print('Salary = ', dict['Salary'])
