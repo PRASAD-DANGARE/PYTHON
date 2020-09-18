803412bc01842147edc301a16ba8faee10f4bd9c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-18 20:36:06 +0530

    Using Student Class

---
+++ student.py
@@ -0,0 +1,36 @@
+# Python Program To Create Student Class And Store It Into student.py
+
+'''
+Function Name    :  Creating Student Class  
+Function Date    :  18 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String
+'''
+
+class student:
+    def setid(self, id):
+        self.id = id
+        
+    def getid(self):
+        return self.id
+    
+    def setname(self, name):
+        self.name = name
+        
+    def getname(self):
+        return self.name
+        
+    def setaddress(self, address):
+        self.address = address
+        
+    def getaddress(self):
+        return self.address
+    
+    def setmarks(self, marks):
+        self.marks = marks
+        
+    def getmarks(self):
+        return self.marks
+    
+        
