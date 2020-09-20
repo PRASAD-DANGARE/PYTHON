9dcf4ebbf13fbfea07aff2b016b9c7248016cc0d
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-20 22:16:43 +0530

    Use Addition Operator On Type Of Object

---
+++ opeoverloading.py
@@ -0,0 +1,26 @@
+# Python Program To Use Addition Operator To Act On Different Types Of Objects
+
+'''
+Function Name    :  Use Addition Operator To Act Different Types Of Object
+Function Date    :  20 Sep 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String
+'''
+
+# Overloading The + Operator
+# Using + On Integer To Add Them
+
+print(10+15)
+
+# Using + On String To Concatenate Them
+
+s1 = "Red"
+s2 = "Fort"
+print(s1+s2)
+
+# Using + On List To Make A Single List
+
+a = [10,20,30]
+b = [5, 15, -10]
+print(a+b)
