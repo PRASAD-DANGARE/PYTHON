6a2208e4b770d6a1162caa39ff33741207d95bd1
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 20:17:14 +0530

    All Program Files Available In Current Directory

---
+++ systemdir.py
@@ -0,0 +1,11 @@
+# Python Program To Display Python Program Files Available In The Current Directory
+
+'''
+Function Name    :  Display Python Program Files Available In Current Directory
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.system('dir * .py') # Execute dir Command Of DOS Operating System
