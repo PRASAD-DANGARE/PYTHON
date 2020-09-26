a3543760f70c9747393edbf1fe870631a7b98ec4
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 20:00:59 +0530

    Rename A Directory

---
+++ rename.py
@@ -0,0 +1,11 @@
+# Python Program To Rename A Directory
+
+'''
+Function Name    :  Rename A Directory
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.rename('enum', 'newenum') # Rename enum As newenum
