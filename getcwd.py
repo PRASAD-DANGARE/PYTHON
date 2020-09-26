0ff9ef216e6218bfcdfe2cb69ed796b1b96167cd
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 19:27:41 +0530

    Know The Currently Working Directory

---
+++ getcwd.py
@@ -0,0 +1,14 @@
+# Python Program To Know The Currently Working Directory
+
+'''
+Function Name    :  Know The Currently Working Directory
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+current = os.getcwd() # Get Current Working Directory
+
+print('Current Directory = ', current)
+
