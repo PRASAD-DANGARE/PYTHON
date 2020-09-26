d2017de09c47ed48b94a75e7488ef3ce873877ff
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 19:40:42 +0530

    makedirs ( )To Create Sub And 2Sub Directories

---
+++ makedirs.py
@@ -0,0 +1,11 @@
+# Python Program To Use The makedirs() Functions To Create Sub And Sub-Sub Directories
+
+'''
+Function Name    :  makedirs()Functions To Create Sub And 2Sub Directories
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.makedirs('newsub/newsub2') # Create Sub And 2Sub Directories
