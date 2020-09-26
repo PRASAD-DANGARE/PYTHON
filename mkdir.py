9164e3dbbeedc453d2d3938d90e65df226a11db2
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 19:34:22 +0530

    Create 2 Sub-Sub Directory In Current Directory

---
+++ mkdir.py
@@ -0,0 +1,15 @@
+# Python Program To Create A Sub Directory And Then Sub-Sub Directory In The Current Directory
+
+'''
+Function Name    :  Create Sub Directory And 2Sub Directory In Current Directory
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.mkdir('mysub') # Create Sub Directory With Name mysub
+
+os.mkdir('mysub/sub2') # Create Sub-Sub Directory By Name mysub2
+
+
