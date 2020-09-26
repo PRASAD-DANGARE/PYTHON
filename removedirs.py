86f5b12ec755ea1cd7b93508b2beb86450a781f5
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 19:57:28 +0530

    Remove A Group Of Directories In The Path

---
+++ removedirs.py
@@ -0,0 +1,11 @@
+# Python Program To Remove A Group Of Directories In The Path
+
+'''
+Function Name    :  Remove A Group Of Directories In The Path
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.removedirs('mysub/mysub2/mysub3') # Remove mysub3 , mysub2 And Then mysub
