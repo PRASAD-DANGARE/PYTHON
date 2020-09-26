3c8cb29131a965a6d73f8462f84b25735ba28dd3
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-09-26 19:51:16 +0530

    Remove Sub Directory Inside Another Directory

---
+++ rmdir.py
@@ -0,0 +1,11 @@
+# Python Program To Remove A Sub Directory A Sub Directory That Is Inside Another Directory
+
+'''
+Function Name    :  Remove Sub Directory Which Inside Another Directory
+Function Date    :  26 Sep 2020
+Function Author  :  Prasad Dangare
+'''
+
+import os
+
+os.rmdir('newsub/newsub2') # Remove newsub2 Directory
