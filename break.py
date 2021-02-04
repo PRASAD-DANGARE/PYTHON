01fc61e8f6416e130a4e47f436a588696564d2d7
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-04 22:47:16 +0530

    Use Of While Loop With Break Statement

---
+++ break.py
@@ -0,0 +1,15 @@
+'''
+Description      :  Use Of While Loop With Break Statement  
+Function Date    :  04 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Str
+Output           :  Str
+'''
+
+while True:
+    s = input('Enter A String : ')
+    if s == 'quit':
+            break
+    print ('Length Of The String Is', len(s))
+print ('Exit The Program')
+
