426fe7a10321a128e74b36c66480b59e2fbdec73
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-04 21:42:57 +0530

    Use Of For Loop

---
+++ for.py
@@ -0,0 +1,15 @@
+'''
+Description      :  Use Of For Loop  
+Function Date    :  04 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+'''
+
+for i in range(1, 5) :
+    
+    print (i)
+
+else :
+ 
+    print ('The for loop is over')
