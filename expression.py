7721c49425196563bfb84eee842141d97f028755
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-04 21:34:29 +0530

    Use Of Expression

---
+++ expression.py
@@ -0,0 +1,15 @@
+'''
+Description      :  Use Of Expression  
+Function Date    :  04 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+'''
+
+length = 5
+breadth = 2
+
+area = length * breadth
+
+print("Area Is : ", area)
+print ("Perimeter Is : ", 2 * (length + breadth))
