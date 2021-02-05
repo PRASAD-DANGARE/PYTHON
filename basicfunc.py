c3a43ae854f599ab55c042227f4847f9e866843a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-05 12:34:40 +0530

    Use Of Function

---
+++ basicfunc.py
@@ -0,0 +1,19 @@
+'''
+Description      :  Use Of Function  
+Function Date    :  05 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Str
+Output           :  Str
+'''
+
+def say_hello():
+     
+     # block belonging to the function
+ 
+    print ('Hello')
+    print ('Im Prasad')
+
+    
+    # End of function
+
+say_hello() # call the function
