fdcf27cd5d665da34f6bda69bd7203ea2fc34820
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-06 15:39:43 +0530

    Module Using Name

---
+++ module_using_name.py
@@ -0,0 +1,15 @@
+'''
+Description      :  Module Using Name 
+Function Date    :  06 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  str
+Output           :  str
+
+'''
+
+if __name__ == '__main__':
+    print ('This program is being run by itself')
+
+else:
+ 
+    print ('I am being imported from another module') # Run By Terminal Then Display This Output
