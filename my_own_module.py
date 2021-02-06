7c86299087bc924f9912681bd4e9811337b3e524
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-06 15:41:24 +0530

    Creating Our Own Module & Import In Next Program
    Importing This Module In my_module_calling.py

---
+++ my_own_module.py
@@ -0,0 +1,14 @@
+'''
+Description      :  Creating Our Own Module & Import In Next Program 
+Function Date    :  06 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  str
+Output           :  str
+
+'''
+
+def Hello():
+
+    print ('Hi, This Is my_own_module Speaking...')
+
+__version__ = '3.x'
