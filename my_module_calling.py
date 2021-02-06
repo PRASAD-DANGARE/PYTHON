29ffd0d0f9d2b2d4127884b3a8d413c703040ac5
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-06 15:42:30 +0530

    Importing Our Own Module Which Is Created
    Importing From my_own_module.py

---
+++ my_module_calling.py
@@ -0,0 +1,13 @@
+'''
+Description      :  Importing Our Own Module Which Is Created 
+Function Date    :  06 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  str
+Output           :  str
+
+'''
+
+import my_own_module
+
+my_own_module.Hello()
+print ('Version', my_own_module.__version__)
