ef10972016ddebfb91ad72047e81702f1328b0c7
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-07 16:49:09 +0530

    Use Of Basic Input / Output

---
+++ io.py
@@ -0,0 +1,13 @@
+'''
+Description      :  Use Of Basic Input / Output  
+Function Date    :  07 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Str
+Output           :  --
+'''
+
+print("Enter One Number") # to display on screen
+no = input() # to accept the standard input device ie keyword
+
+print ("Your Number Is : ", no)
+print ("Data Type Of Give Number Is : ", type(no))
