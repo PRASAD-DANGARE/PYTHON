34657335db3f0242efcb7a3cb5760c287f3e451d
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-07 16:47:24 +0530

    Use Of Numeric Data Type

---
+++ numeric.py
@@ -0,0 +1,30 @@
+'''
+Description      :  Use Of Numeric Data Type 
+Function Date    :  07 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int, Str, Complex
+Output           :  --
+'''
+
+# Numeric DataType int, float, complex
+
+no = 11 # integer
+
+print ("Value Of No is", no)
+print ("Data Type Of No is", type(no))
+print ("Unique Id Of No is", id(no)) # address
+print("\n")
+
+value = 11.45 # float
+
+print ("Value Of No is", value)
+print ("Data Type Of Value is", type(value))
+print ("Unique Id Of Value is", id(value))
+print("\n")
+
+a = 4+5j # complex no
+
+print ("Value Of a is", a)
+print ("Data Type Of a is", type(a))
+print ("Unique Id Of a is", id(a))
+print("\n")
