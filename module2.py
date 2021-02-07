297725ecb3c0549176deaeb62be718986d7fb783
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-07 16:57:06 +0530

    Creating Module For Addition Of Two Numbers
    This File Contain Function For module1.py

---
+++ module2.py
@@ -0,0 +1,13 @@
+'''
+Description      :  Creating Module For Addition Of Two Numbers
+                 :  This File Contain Function For module1.py
+
+Function Date    :  07 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  --
+Output           :  --
+'''
+
+def Addition(value1, value2):
+    ret = value1 + value2 # local 4 
+    return ret
