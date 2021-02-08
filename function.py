2c94792c49803ea0e183fc786860a9561ec1f1c8
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-08 21:15:23 +0530

    Performing New Variations In Functions

--- function.py
+++ function.py
@@ -1,22 +1,77 @@
-# Python Program To Know How To Define A Function Inside Another Function
-
 '''
-Function Name    :  Defining A Function Inside Another Function. 
-Function Date    :  6 Sep 2020
+Description      :  Performing New Variations In Functions  
+Function Date    :  08 Feb 2021
 Function Author  :  Prasad Dangare
-Input            :  String
-Output           :  String 
+Input            :  Str
+Output           :  Str
 '''
 
-def display(str):
-    def message():
-        print("\n")
-        return 'How Are You '
+print("Demonstration Of Advanced Functions ")
+
+# 1 : Function Which Accepts Nothing And Return Nothing
+
+def Marvellous1():
+    print("Inside Marvellous1")
     
-    result = message()+str
-    return result
 
-# Call Display() Function
+# 2 : Function Which Accepts Parameters And Returns Nothing
 
-print(display("Prasad"))
-print("\n")
+def Marvellous2(no):
+    print("Inside Marvellous2 ")
+    print("Accepted Value Is :", no)
+
+    
+# 3 : Function Which Accepts Parameter And Return The Value
+
+def Marvellous3(no):
+    print("Inside Marvellous3 ")
+    print("Accepted Value Is :", no)
+    return no + 1
+
+    
+# 4 : Function Which Accepts Multiple Values And Return Multiple Values
+
+def Marvellous4(value1, value2):
+    print("Inside Marvellous4 ")
+    add = value1 + value2
+    sub = value1 - value2
+    return add, sub
+
+
+# 5 : Function Which Calls Another Function Which Is Defined Outside It
+
+def Marvellous5():
+    print("Inside Marvellous5 ")
+    Marvellous1()
+
+
+# 6 : Function Which Contain Another Nested Function Defined In It
+
+def Marvellous6():
+    print("Inside Marvellous6")
+    
+    def InnerFun():
+        print("Inside Inner Fun")
+    InnerFun()
+    
+    
+def main():
+    no = 11
+    
+    Marvellous1()
+    
+    Marvellous2(no)
+
+    ret = Marvellous3(no)
+    print("Return Value Is : ",ret)
+
+    ret1, ret2 = Marvellous4(10, 4)
+    print("Addition is : ",ret1)
+    print("Substrin is : ",ret2)
+
+    Marvellous5()
+
+    Marvellous6()
+    
+if __name__ == "__main__":
+    main()
