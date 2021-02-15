71650952f14ea26cfb945c6c94311f15a19cc08c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-15 11:13:19 +0530

    Find Whether The Given Number Is Odd, Even
     Creating Module Which Shows That Given Number Is Odd Or Even, Display The Output In Selection1.py

---
+++ selection.py
@@ -0,0 +1,23 @@
+'''
+Description      :  Creating Module Which Shows That Given Number Is Odd Or Even 
+Function Date    :  15 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Display The Output In Selection1.py
+'''
+
+# Demonstration of selection
+
+'''no = 11
+
+if no % 2 == 0:
+    print("no is even")
+else:
+    print("no is odd")
+'''
+
+def CheckEven(No):
+    if No % 2 == 0:
+        return True
+    else:
+        return False
