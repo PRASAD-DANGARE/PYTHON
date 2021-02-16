f0e3ee6a5214d637df7674848c24262bb3593119
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-16 12:25:11 +0530

    Demonstration Of if, elif, else Statement

---
+++ if_elif_else.py
@@ -0,0 +1,17 @@
+'''
+Description      :  Demonstration Of if, elif, else Statement 
+Function Date    :  16 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+no = 12
+
+if no > 11:
+    print("It is positive number")
+elif no == 0:
+    print("It is zero")
+else:
+    print("It is negative number")
