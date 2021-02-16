8374a002de879cdb8cc25225dead52beac63b8e8
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-16 12:19:56 +0530

    Demonstration Of Whileloop

---
+++ whileloop.py
@@ -0,0 +1,16 @@
+'''
+Description      :  Demonstration Of Whileloop 
+Function Date    :  16 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+'''
+
+
+print("Demonstration of While Loop ")
+
+i = 1
+
+while(i<5):
+    print(i)
+    i = i + 1
