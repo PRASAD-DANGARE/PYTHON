a02cf495a1c3be5c5dc86ae0024fbb1bdf232142
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-16 12:23:39 +0530

    Demonstration Of if & else Statements

---
+++ if_else.py
@@ -0,0 +1,14 @@
+'''
+Description      :  Demonstration Of if & else 
+Function Date    :  16 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+no = 12
+if no >= 0:
+    print("It is positive number")
+else:
+    print("It is negative number")
