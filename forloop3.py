a2606eb3c03901d61dbba0b1706bfe3ffd2f3084
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-16 12:18:41 +0530

    Demonstration Of Forloop In List

---
+++ forloop3.py
@@ -0,0 +1,18 @@
+'''
+Description      :  Demonstration Of Forloop In List 
+Function Date    :  16 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Hetrogenious elements
+Output           :  --
+'''
+
+
+print("Demonstration of For Loop")
+
+x = [10,20,"Abc","xyz",30.1]
+
+for i in x:
+    print(i)
+
+for i in range(0,9):
+    print(i)
