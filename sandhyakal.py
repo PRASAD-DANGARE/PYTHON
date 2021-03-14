fa3182d34153b5a2c36cc4cad61e5d7887797816
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-14 23:32:25 +0530

    Multithreading

---
+++ sandhyakal.py
@@ -0,0 +1,45 @@
+'''
+Description      :  Multithreading
+Function Date    :  14 Mar 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+import os
+import threading
+from time import sleep
+
+def Thread1(no):
+    print("Thread1 is created")
+    print("PID of Thread1 is : ",os.getpid())
+    for i in range(no):
+        sleep(1)
+        print("Thread-1 ",i)
+
+def Thread2(no):
+    print("Thread2 is created")
+    print("PID of Thread2 is : ",os.getpid())
+    for i in range(no):
+        sleep(1)
+        print("Thread-2 ",i)
+
+def main():
+    print("Inside main thread")
+    print("PID of main process is :",os.getpid())
+    print("PID of parent process of main is :",os.getppid())
+    value = 10
+    t1 = threading.Thread(target = Thread1, args = (value,))
+    t2 = threading.Thread(target = Thread2, args = (value,))
+    
+    t1.start()
+    t2.start()
+    
+    t1.join()
+    t2.join()
+    
+    print("End of main thread")
+    
+if __name__ == "__main__":
+    main()
