fb9d561ba9e39116d8bdec69027d1b6dbee77b27
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-24 15:05:52 +0530

    Demonstration of Serial Processing

---
+++ Cpu1.py
@@ -0,0 +1,35 @@
+'''
+Function Name    :  SerialProcessing()
+Description      :  Demonstration of Serial Processing
+Function Date    :  14 Mar 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+import os
+import time
+
+def Square(no):
+    return no * no
+
+def SerialProcessing():
+    start = time.time()
+    print("Serial Processing")
+    arr = range(100000)
+    ret = []
+
+    for i in arr:
+        ret.append(Square(i))
+    #print("Result of serial processing is ",ret)
+    end = time.time()
+    print("Time requred for serial execution", end-start)
+
+def main():
+
+    print("Inside main")
+    SerialProcessing()
+
+if __name__ == "__main__":
+    main()
