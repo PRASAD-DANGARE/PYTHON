0b813e1c2112cd6e264c1061d48fd7a4f00cd024
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-04-09 20:03:37 +0530

    Demonstration of dictionary

---
+++ Dictionary.py
@@ -0,0 +1,23 @@
+'''
+Function Name    :  main()
+Description      :  Demonstration of dictionary  
+Function Date    :  21 Mar 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+def main():
+    
+    batches = {"PPA":12500, "LB":11000, "Python": 13000, "Angular":10000}
+
+    print(type(batches))
+    print(len(batches))
+
+    print(batches)
+
+    print(batches["PPA"]) 
+
+if __name__ == "__main__":
+    main()
